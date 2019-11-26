e4d7d9c6bc0999d62bfd99522528fee998b06322
olivierguinel <olivierguinel@gmail.com>
2019-11-26 10:53:52 +0100

    seed 2.0: ajout Orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
 user_count = 0
 10.times do
-  User.create(email: Faker::Internet.email, password: Faker::Internet.password)
-  user_count += 1
+	User.create(email: Faker::Internet.email, password: Faker::Internet.password)
+	user_count += 1
 end
 puts "#{user_count} users created"
 
-
+Item.destroy_all
 item_count = 0
 10.times do
-  Item.create(title: Faker::Book.title, description: "test", price: 50, image_url: Faker::Internet.url)
-  item_count += 1
+	Item.create(title: Faker::Book.title, description: "test", price: 50, image_url: Faker::Internet.url)
+	item_count += 1
 end
 puts "#{item_count} items created"
+
+Order.destroy_all
+order_count = 0
+20.times do
+	Order.create(user_id: User.all.sample.id)
+	order_count += 1
+end
+puts "#{order_count} orders created"
+
+OrderedItem.destroy_all
+ordereditem_count = 0
+50.times do
+	OrderedItem.create(order_id: Order.all.sample.id, item_id: Item.all.sample.id, quantity: rand(1..5))
+	ordereditem_count += 1
+end
+puts "#{ordereditem_count} ordered item created"
