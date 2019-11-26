d0672bfd31f58177625d2e6c2dba2918e7cb2fb6
Greutz <Gorton83@gmail.com>
2019-11-26 16:11:56 +0100

    [PROGRESS] basic cart view/controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'page_statics#index'
   devise_for :users
   resources :items
-  resources :users
+  resources :users do
+    resources :carts
+  end
   resources :orders
   resources :ordereditems
   resources :charges
