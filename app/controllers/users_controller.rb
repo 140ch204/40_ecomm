7b659c2be97633f3d4b20ca7a711005212832500
olivierguinel <olivierguinel@gmail.com>
2019-11-28 17:52:38 +0100

    authentification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!, only: [:index, :show, :edit, :update, :destroy]
 	before_action :is_admin?, only: [:index]
+	before_action :current_user, only: [:index, :show, :edit, :update, :destroy]
+
 
 	def index
 		@users = User.all
@@ -27,11 +29,18 @@ class UsersController < ApplicationController
 	
 	private
 
-	  def is_admin?
-	    if !current_user.nil? && current_user.admin = true
-	    	return true
-	    	flash[:notice] = "You can access admins' pages."
-	    end
-	  end
+	def is_admin?
+		if !current_user.nil? && current_user.admin = true
+			return true
+			flash[:notice] = "You can access admins' pages."
+		end
+	end
+
+	def current_user
+		if current_user.id != User.find(params[:id]).id
+			flash[:notice] = "You can't see that."
+			redirect_to root_path
+		end
+	end
 
 end
