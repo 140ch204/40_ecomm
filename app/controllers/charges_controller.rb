03cfd9a2f9b11ed62e1ecb712361f2393caa5e19
olivierguinel <olivierguinel@gmail.com>
2019-11-28 11:22:38 +0100

    [FINISH] views order #index #show

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,8 +5,6 @@ class ChargesController < ApplicationController
 	after_action :clean_cart, only: [:create]
 
 	def new
-		puts "X"*50
-		puts params
 		@user = current_user
 		@cart = @user.cart
 		@cart_items = CartElement.where(cart_id: @cart.id)
@@ -17,8 +15,6 @@ class ChargesController < ApplicationController
 	end
 
 	def create
-		puts "X" *50
-		puts params
 		@amount = params[:amount]
 		customer = Stripe::Customer.create({
 			email: params[:stripeEmail],
@@ -39,20 +35,34 @@ class ChargesController < ApplicationController
 		redirect_to new_charge_path
 	end
 
+	private
+
 	def generate_order
-		order = Order.create(user_id: current_user.id)
-		return order.id
+		orderx = Order.create(user_id: current_user.id)
+		puts "X"*100
+		puts "0"*100
+		puts order_id = orderx.id
+		puts "X"*100
+		return order_id
 	end
 
 	def generate_ordered_elements
 		@user = current_user
 		@cart = @user.cart
 		@cart_items = CartElement.where(cart_id: @cart.id)
+		@order = Order.last.id
+		puts "X"*100
+		puts @order
+		puts "X"*100
 		@cart_items.each do |item|
+			
 			puts "X"*100
-			puts order.id
+			puts item.id
+			puts item.quantity
+
 			puts "X"*100
-			OrderedItem.create(order_id: order.id,
+
+			OrderedItem.create(order_id: @order,
 				item_id: item.id,
 				quantity: item.quantity)
 		end
