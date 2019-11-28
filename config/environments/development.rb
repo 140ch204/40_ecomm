36b817d752ccbc0d868f94c595a89ccbe6dd7b80
140ch204 <140ch204_github@gadz.org>
2019-11-28 23:43:01 +0100

    mailjet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # Letter opener to test mailers in dev env.
-  config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.delivery_method = :mailjet_api
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,9 +92,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :mailjet
+  
+  config.action_mailer.delivery_method = :mailjet_api
   config.action_mailer.perform_deliveries = true
-
 end
 
 
