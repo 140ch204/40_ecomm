6aef86435718856d7be0ad340d65351094c6ddde
Greutz <Gorton83@gmail.com>
2019-11-28 10:13:23 +0100

    fix

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,70 @@
+class ChargesController < ApplicationController
+
+	after_action :generate_ordered_elements, only: [:create]
+	after_action :generate_order, only: [:create]	
+	after_action :clean_cart, only: [:create]
+
+	def new
+		puts "X"*50
+		puts params
+		@user = current_user
+		@cart = @user.cart
+		@cart_items = CartElement.where(cart_id: @cart.id)
+		@amount = 0
+		@cart_items.each do |item|
+			@amount += Item.find(item.item_id).price.to_i * item.quantity
+		end
+	end
+
+	def create
+		puts "X" *50
+		puts params
+		@amount = params[:amount]
+		customer = Stripe::Customer.create({
+			email: params[:stripeEmail],
+			source: params[:stripeToken],
+		})
+
+		charge = Stripe::Charge.create({
+			customer: customer.id,
+			amount: @amount,
+			description: "Paiement de #{current_user.email}",
+			currency: 'eur',
+		})
+
+		redirect_to orders_path
+
+	rescue Stripe::CardError => e
+		flash[:error] = e.message
+		redirect_to new_charge_path
+	end
+
+	def generate_order
+		order = Order.create(user_id: current_user.id)
+		return order.id
+	end
+
+	def generate_ordered_elements
+		@user = current_user
+		@cart = @user.cart
+		@cart_items = CartElement.where(cart_id: @cart.id)
+		@cart_items.each do |item|
+			puts "X"*100
+			puts order.id
+			puts "X"*100
+			OrderedItem.create(order_id: order.id,
+				item_id: item.id,
+				quantity: item.quantity)
+		end
+	end
+
+	def clean_cart
+		@user = current_user
+		@cart = @user.cart
+		@cart_items = CartElement.where(cart_id: @cart.id)
+		@cart_items.each do |item|
+			item.destroy
+		end
+	end
+
+end
